c1b62758938e3d76c9f2f030b492883bac87b7bc
s01dier-0f-g0d <147149708+s01dier-0f-g0d@users.noreply.github.com>
2025-11-01 19:52:17 +0530

    Delete base directory

--- base/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm as m
-from .models import Destination
-
-class DestinationForm(m):
-    class Meta:
-        model=Destination
-        fields=['name','country','description','average_cost','rating']
